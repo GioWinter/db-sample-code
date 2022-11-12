bbc1daa4c3e9ca9605e1fc2d1b2162b71bb2375d
Giovani Winter <117488257+GioWinter@users.noreply.github.com>
2022-11-12 20:21:32 -0300

    Update script_01_regions_v1.0.sql
    alteração do nome regions para continents.

--- Location/script_01_regions_v1.0.sql
+++ Location/script_01_regions_v1.0.sql
@@ -1,16 +1,16 @@
 --- Creat By...: Giovani Pacheco
 --- Version....: 1.0
 --- Create Date: 11/12/2022
---- Description: table containing the continents.
+--- Updates....: 
 
 
 ---------------------------------------------------------------------------------------------------------
 -- Table: REGION
 ---------------------------------------------------------------------------------------------------------
 
-CREATE TABLE LOC.REGION (
-	ID					INT NOT NULL,          -- Unique identifier
-	REGION_NAME			VARCHAR2(50) NOT NULL          -- Name of Region
+CREATE TABLE LOC.CONTINENTS (
+	ID		   INT NOT NULL,          -- Unique identifier
+	CONTINENT_NAME	   VARCHAR2(50) NOT NULL  -- Name of CONTINENT
 );
 
 
@@ -23,11 +23,11 @@ ALTER TABLE LOC.REGION
 -- DML: REGION 
 ---------------------------------------------------------------------------------------------------------
 
-INSERT INTO LOC.REGION (ID,REGION_NAME) VALUES (1,'Africa');
-INSERT INTO LOC.REGION (ID,REGION_NAME) VALUES (2,'Americas');
-INSERT INTO LOC.REGION (ID,REGION_NAME) VALUES (3,'Antarctica');
-INSERT INTO LOC.REGION (ID,REGION_NAME) VALUES (4,'Asia');
-INSERT INTO LOC.REGION (ID,REGION_NAME) VALUES (5,'Europa');
-INSERT INTO LOC.REGION (ID,REGION_NAME) VALUES (6,'Aoceania');
+INSERT INTO LOC.REGION (ID,CONTINENT_NAME) VALUES (1,'Africa');
+INSERT INTO LOC.REGION (ID,CONTINENT_NAME) VALUES (2,'Americas');
+INSERT INTO LOC.REGION (ID,CONTINENT_NAME) VALUES (3,'Antarctica');
+INSERT INTO LOC.REGION (ID,CONTINENT_NAME) VALUES (4,'Asia');
+INSERT INTO LOC.REGION (ID,CONTINENT_NAME) VALUES (5,'Europa');
+INSERT INTO LOC.REGION (ID,CONTINENT_NAME) VALUES (6,'Aoceania');
 
 
