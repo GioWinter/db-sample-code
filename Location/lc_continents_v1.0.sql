3a7018df1cec40dc7c5110f913734df63e8a9614
Giovani Winter <117488257+GioWinter@users.noreply.github.com>
2022-11-14 19:47:43 -0300

    Update lc_continents_v1.0.sql

--- Location/lc_continents_v1.0.sql
+++ Location/lc_continents_v1.0.sql
@@ -9,26 +9,25 @@
 ---------------------------------------------------------------------------------------------------------
 
 CREATE TABLE LOC.CONTINENTS (
-	ID		   INT NOT NULL,          -- Unique identifier
+	CONTINENT_ID	   INT NOT NULL,          -- Unique identifier
 	CONTINENT	   VARCHAR2(50) NOT NULL  -- Continent Name
 );
 
 
 --- CONSTRAINTS
 ALTER TABLE LOC.CONTINENTS
-	ADD CONSTRAINT PK001_REGION PRIMARY KEY (ID);
+	ADD CONSTRAINT PK001_CONTINENT PRIMARY KEY (CONTINENT_ID);
 	
 	
 ---------------------------------------------------------------------------------------------------------
 -- DML: CONTINENTS 
 ---------------------------------------------------------------------------------------------------------
 
-INSERT INTO LOC.CONTINENTS (ID,CONTINENT) VALUES (10,'Africa');
-INSERT INTO LOC.CONTINENTS (ID,CONTINENT) VALUES (20,'Americas');
-INSERT INTO LOC.CONTINENTS (ID,CONTINENT) VALUES (30,'Antarctica');
-INSERT INTO LOC.CONTINENTS (ID,CONTINENT) VALUES (40,'Asia');
-INSERT INTO LOC.CONTINENTS (ID,CONTINENT) VALUES (50,'Europa');
-INSERT INTO LOC.CONTINENTS (ID,CONTINENT) VALUES (60,'Aoceania');
-
+INSERT INTO LOC.REGIONS (CONTINENT_ID,CONTINENT) VALUES (10,'Africa');
+INSERT INTO LOC.REGIONS (CONTINENT_ID,CONTINENT) VALUES (20,'Americas');
+INSERT INTO LOC.REGIONS (CONTINENT_ID,CONTINENT) VALUES (30,'Asia');
+INSERT INTO LOC.REGIONS (CONTINENT_ID,CONTINENT) VALUES (40,'Antarctica');
+INSERT INTO LOC.REGIONS (CONTINENT_ID,CONTINENT) VALUES (50,'Europe');
+INSERT INTO LOC.REGIONS (CONTINENT_ID,CONTINENT) VALUES (60,'Oceania');
 
 
